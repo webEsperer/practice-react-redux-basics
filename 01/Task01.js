e0d7c3b5c75f24040a7c3bec4ef49968b7906980
Piotr Matejek <webesperer@gmail.com>
2024-11-04 06:01:04 +0100

    solved task01

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Subject from './../src/libs/Subject';
 
+const subject = new Subject();
+
+
 const Task01 = () => {
     const [time, setTime] = useState(0);
     const [tagName, setTagName] = useState('??');
@@ -18,14 +21,16 @@ const Task01 = () => {
         setCursorPosition(`[${pageX},${pageY}]`);
     }
 
+   subject.subscribe(setEventTime);
+   subject.subscribe(setEventTagName);
+   subject.subscribe(setEventCursorPosition);
+
     return (
         <section>
             <h1>Task 1</h1>
             
             <div onClick={ event => {
-                setEventTime(event);
-                setEventTagName(event);
-                setEventCursorPosition(event);
+                subject.notify(event);
             }}>
                 <p>
                     <strong>Kliknij wybrany element:</strong> <a>link</a>, <button>button</button>, <span>span</span>
