f1bcc5a567d0e1e10ad4c11bb5bb73b051a7aab7
Piotr Matejek <webesperer@gmail.com>
2024-11-05 05:02:35 +0100

    solved task03

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,31 @@ import Task03 from "./../../03/Task03";
 import Task04 from "./../../04/Task04";
 import Task05 from "./../../05/Task05";
 
-const store = createStore(() => {
-  return {
-    message: "dziala",
-  };
-});
+const initState = {
+  message: "dziala",
+  time: new Date(),
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case "getCurrentTime":
+      return { ...state, time: new Date() };
+  }
+
+  return state;
+};
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
   return (
     <Provider store={store}>
       <Task01 />
       <Task02 />
-      {/* <Task03 /> */}
+      <Task03 />
       {/* <Task04 /> */}
       {/* <Task05 /> */}
     </Provider>
